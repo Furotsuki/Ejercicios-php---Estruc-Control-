de3f0b0bf9ef9504e07fb70d12bae481e68a9a04
jpdazab <117194628+Furotsuki@users.noreply.github.com>
2024-02-29 19:09:11 -0500

    Saber si un numero es primo o no
    9.	Hacer una página web que permita capturar en una caja de texto un número e imprima en texto si es primo o no.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <form method="post">
-            Digite una palabra<input type="text" name="numero1" autofocus/>
+            Digite un numero<input type="number" name="numero1" autofocus/>
 
             <input name="enviar" type="submit" value="Enviar"/> 
 
@@ -15,42 +15,26 @@
         <?php
         if (isset($_POST['enviar'])) {
 
-            $numero1 = $_POST['numero1'];
+            $numero1 = intval($_POST['numero1']);
+            echo gettype($numero1);
             echo '<fieldset><legend>Procedimientos</legend>';
             sacara_numero($numero1);
             echo '</fieldset>';
         }
 
         function sacara_numero($frase) {
-
-            // Contar vocales en mayúsculas
-            $vocales_mayusculas = str_split('AEIOU');
-            $contador_mayusculas = array_fill_keys($vocales_mayusculas, 0);
-
-            // Contar vocales en minúsculas
-            $vocales_minusculas = str_split('aeiou');
-            $contador_minusculas = array_fill_keys($vocales_minusculas, 0);
-
-            // Procesar cada letra de la frase
-            for ($i = 0; $i < strlen($frase); $i++) {
-                $letra = $frase[$i];
-                if (in_array($letra, $vocales_mayusculas)) {
-                    $contador_mayusculas[$letra]++;
-                } 
-                if (in_array($letra, $vocales_minusculas)) {
-                    $contador_minusculas[$letra]++;
+            $contar = 0;
+            for ($i = 1; $i <= $frase; $i++) {
+                if ($frase % $i == 0) {
+                    $contar++;
                 }
             }
-
-            // Imprimir resultados
-            echo "<h2>Vocales en Mayúsculas:</h2>";
-            foreach ($contador_mayusculas as $vocal => $cantidad) {
-                echo "$vocal: $cantidad<br>";
-            }
-
-            echo "<h2>Vocales en Minúsculas:</h2>";
-            foreach ($contador_minusculas as $vocal => $cantidad) {
-                echo "$vocal: $cantidad<br>";
+            if ($contar > 2 or $contar < 2) {
+                echo 'El numero ' . $frase . ' no es primo';
+            } else {
+                if ($contar == 2) {
+                    echo 'El numero ' . $frase . ' es primo';
+                }
             }
         }
         ?>
